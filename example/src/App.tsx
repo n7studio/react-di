bf2e311c6cbe4422a3763e925de6c25d7c893725
clerenscripts <clerenscripts@nine7soft.com>
2021-06-13 20:29:37 -0400

    add react-di to example from git

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,13 +5,13 @@ import { useDependency } from 'react-di'
 
 function App() {
 
-  const context = useDependency('hello')
-  console.log(context)
+  const hello = useDependency('hello')
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       { context.hello()}
+        {hello}
       </header>
     </div>
   );
